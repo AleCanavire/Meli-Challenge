7c1d546def455631acbfd4b103c144019f8bcd6f
AleCanavire <ale.canavire1@gmail.com>
2023-02-19 15:24:59 -0300

    Nav and Home responsive

--- src/components/ItemListContainer/ItemList.js
+++ src/components/ItemListContainer/ItemList.js
@@ -1,43 +1,33 @@
 import React from "react";
 import Item from "./Item";
 import ItemSkeleton from "./ItemSkeleton";
-import stitchProduct from "../../data/stitchProduct";
 import CarouselItems from "../ReactSlick/CarouselItems";
 
 function ItemList({ products }) {
-  let numbers = [];
-  for (let i = 1; i <= 20; i++) {
-    numbers.push(i)
-  }
-
-  if (products === 0) {
-    return (
-      <div className="itemsContainer">
-        {numbers.map((number) => {
-          return (
-            <ItemSkeleton key={number}/>
-          );
-        })}
-      </div>
-    )
-  }
 
   return (
     <div className="itemsContainer">
-      <CarouselItems number={5}>
-        {products.map((product) => {
+      { products === 0
+      ? [1,2,3,4,5].map((number) => {
           return (
-            <Item
-              key={product.id}
-              url={product.id}
-              title={product.title}
-              img={product.image || product.pictures[0].url}
-              price={product.price}
-            />
+            <ItemSkeleton key={number}/>
           );
-        })}  
-      </CarouselItems>
+        })
       
+      : <CarouselItems number={5}>
+          {products.map((product) => {
+            return (
+              <Item
+                key={product.id}
+                url={product.id}
+                title={product.title}
+                img={product.image || product.pictures[0].url}
+                price={product.price}
+              />
+            );
+          })}  
+        </CarouselItems>
+      }
     </div>
   );
 }
