0a7127b0b7678d624f59fe5e2a1c569f7d287ed4
AleCanavire <ale.canavire1@gmail.com>
2023-03-05 23:11:49 -0300

    Navbar Dark Theme

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -8,8 +8,8 @@ import { ReactComponent as LocationIcon } from '../../assets/img/location.svg';
 function NavBar() {
   const [shadowActive, setShadow] = useState(false);
   
-  function shadowEffect() {
-    setShadow(!shadowActive)
+  function shadowEffect(state) {
+    setShadow(state)
   }
 
   const searchText = useResize(1200)
@@ -39,51 +39,61 @@ function NavBar() {
               </button>
             </form>
           </div>
-          <div className='categories'>
-            <ul>
-              <li onMouseEnter={shadowEffect} onMouseLeave={shadowEffect}>
-                <span>Categorías</span>
-                <div className='allCategories'>
-                  <span>Vehículos</span>
-                  <span>Inmuebles</span>
-                  <span>Supermercado</span>
-                  <Link to='/category/tecnología'>Tecnología</Link>
-                  <Link to='/category/joyería'>Joyeria</Link>
-                  <Link to='/category/ropa-hombre'>Ropa de Hombre</Link>
-                  <Link to='/category/ropa-mujer'>Ropa de Mujer</Link>
-                  <span>Herramientas</span>
-                  <span>Construcción</span>
-                  <span>Deportes y Fitness</span>
-                  <span>Accesorios para Vehiculos</span>
-                  <span>Moda</span>
-                  <span>Juegos y Juguetes</span>
-                  <span>Bebés</span>
-                  <span>Belleza y Cuidado Personal</span>
-                  <span>Salud y Equipamento Médico</span>
-                  <span>Industrias y Oficinas</span>
-                  <span>Productos Sustentables</span>
-                  <span>Servicios</span>
-                  <span>Más Vendidos</span>
-                  <span>Tiendas oficiales</span>
-                  <span>Ver más categorias</span>
-                </div>
-              </li>
-              <li><span>Ofertas</span></li>
-              <li><span>Historial</span></li>
-              <li><span>Supermercado</span></li>
-              <li><span>Moda</span></li>
-              <li><span>Vender</span></li>
-              <li><span>Ayuda</span></li>
-            </ul>
-            <ThemeToggle/>
-          </div>
-          <div className='disney'></div>
+          { useResize(1200) &&
+            <div className='categories'>
+              <ul>
+                <li onMouseOver={()=>shadowEffect(true)} onMouseOut={()=>shadowEffect(false)}>
+                  <span>Categorías</span>
+                  { shadowActive &&
+                    <div className="allCategories">
+                      <div className="allCategoriesContent">
+                        <span>Vehículos</span>
+                        <span>Inmuebles</span>
+                        <span>Supermercado</span>
+                        <Link to='/category/tecnología'>Tecnología</Link>
+                        <Link to='/category/joyería'>Joyeria</Link>
+                        <Link to='/category/ropa-hombre'>Ropa de Hombre</Link>
+                        <Link to='/category/ropa-mujer'>Ropa de Mujer</Link>
+                        <span>Herramientas</span>
+                        <span>Construcción</span>
+                        <span>Deportes y Fitness</span>
+                        <span>Accesorios para Vehiculos</span>
+                        <span>Moda</span>
+                        <span>Juegos y Juguetes</span>
+                        <span>Bebés</span>
+                        <span>Belleza y Cuidado Personal</span>
+                        <span>Salud y Equipamento Médico</span>
+                        <span>Industrias y Oficinas</span>
+                        <span>Productos Sustentables</span>
+                        <span>Servicios</span>
+                        <span>Más Vendidos</span>
+                        <span>Tiendas oficiales</span>
+                        <span>Ver más categorias</span>
+                      </div>
+                    </div>
+                  }
+                </li>
+                <li><span>Ofertas</span></li>
+                <li><span>Historial</span></li>
+                <li><span>Supermercado</span></li>
+                <li><span>Moda</span></li>
+                <li><span>Vender</span></li>
+                <li><span>Ayuda</span></li>
+              </ul>
+              <ThemeToggle/>
+            </div>
+          }
+          { useResize(1200) && <div className='disney'></div> }
           <div className='account'>
             <ul>
-              <li className='accountOptions'><span>Creá  tu cuenta</span></li>
-              <li className='accountOptions'><span>Ingresá</span></li>
-              <li className='accountOptions'><span>Mis compras</span></li>
-              <li className='burgerButton'><span></span></li>
+              { useResize(1200)
+              ? <>
+                <li className='accountOptions'><span>Creá  tu cuenta</span></li>
+                <li className='accountOptions'><span>Ingresá</span></li>
+                <li className='accountOptions'><span>Mis compras</span></li>
+                </>
+              : <li className='burgerButton'><span></span></li>
+              }
               <li className='cartButton'><CartWidget/></li>
             </ul>
           </div>
@@ -91,7 +101,7 @@ function NavBar() {
       </nav>
       <div className={`shadow${shadowActive ? " shadowActive" : ""}`}></div>
     </>
-    )
+  )
 }
 
 export default NavBar;
